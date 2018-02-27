350180881d91b9fed70e22a8bf6edd8ab1d1be21
pedped <convertersoft@gmail.com>
2018-02-27 10:52:21 +0330

    link updated

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -11,7 +11,7 @@ rm -rf fribidi-*
 rm -rf lame-*
 
 wget -O- https://ftp.osuosl.org/pub/blfs/conglomeration/libpng/libpng-1.6.21.tar.xz | tar xJ
-wget -O- https://github.com/libexpat/libexpat/releases/download/R_2_2_5/expat-2.2.5.tar.bz2 | tar xz
+wget -O- http://downloads.sourceforge.net/expat/expat-2.1.0.tar.gz | tar xz
 wget -O- https://ftp.osuosl.org/pub/blfs/conglomeration/fribidi/fribidi-0.19.7.tar.bz2 | tar xj
 wget -O- https://ftp.osuosl.org/pub/blfs/conglomeration/lame/lame-3.99.5.tar.gz | tar xz
 echo "============================================"
